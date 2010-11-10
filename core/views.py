00ee69fd22eff4355bcd20569ecad2d7828d6f9c
Lech Twaróg <lech.twarog@gmail.com>
2010-11-10 10:16:49 +0100

    orbited added

--- core/views.py
+++ core/views.py
@@ -5,63 +5,26 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 
 from cumulumbus.core.models import Post
-from cumulumbus.service.lastfm.models import LastfmAccount
-from cumulumbus.service.lastfm.fetcher import LastfmFetcher
-from cumulumbus.service.rss.models import RSSAccount
-from cumulumbus.service.rss.fetcher import RSSFetcher
 
 def index( request ):
-	account, created = LastfmAccount.objects.get_or_create( service = 'LastfmService', username = 'robinhooj' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-	
-	fetcher = LastfmFetcher( account )
-	fetcher.run();
-	
-	account, created = RSSAccount.objects.get_or_create( service = 'RSSService', url = 'http://feeds.feedburner.com/ajaxian' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-
-	fetcher = RSSFetcher( account )
-	fetcher.run()
-
-	account, created = RSSAccount.objects.get_or_create( service = 'RSSService', url = 'http://legia.com/www/view_rss.php' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-
-	fetcher = RSSFetcher( account )
-	fetcher.run()
-	
-	account, created = RSSAccount.objects.get_or_create( service = 'RSSService', url = 'http://feeds.feedburner.com/ps3site-pl' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-
-	fetcher = RSSFetcher( account )
-	fetcher.run()
-
-	account, created = RSSAccount.objects.get_or_create( service = 'RSSService', url = 'http://www.rmf24.pl/feed' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-
-	fetcher = RSSFetcher( account )
-	fetcher.run()
-
-	account, created = RSSAccount.objects.get_or_create( service = 'RSSService', url = 'http://filmaster.pl/planeta/rss' );
-	if created:
-		account.last_import = datetime.now()
-		account.save()
-
-	fetcher = RSSFetcher( account )
-	fetcher.run()
-
 	items = Post.objects.filter( readed = False ).order_by( '-date_added' )[:50]
 	
 	return render_to_response( "index.html", { "items": items }, context_instance = RequestContext( request ) )
+
+def test_stomp( request ):
+	import stomp
+	import json
+
+	conn = stomp.Connection( host_and_ports = [ ('127.0.0.1', 13131314) ] ) 
+	conn.start()
+	conn.connect()
+	conn.subscribe( destination='/posts', ack='auto' )
+	
+	post = Post.objects.all()[0]
+	msg = json.dumps( { 'post': { 'id': post.id }  } )
+
+	conn.send( msg, destination='/posts' )
+	return HttpResponse( 'OK' )
 
 def mark_as_readed( request, id ):
 	post = get_object_or_404( Post, id = id )
--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ import os
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
-from cumulumbus.core.views import index, mark_as_readed
+from cumulumbus.core.views import index, mark_as_readed, test_stomp
 
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
 	( r'^media/(.*)$', 'django.views.static.serve', { 'document_root' : os.path.join( os.path.dirname( __file__ ), 'media' ) } ),
 	( r'^$', index ),
+	( r'^test$', test_stomp ),
 	( r'^admin/', include( admin.site.urls ) ),
 	( r'readed/(?P<id>\d+)', mark_as_readed ),
 )
